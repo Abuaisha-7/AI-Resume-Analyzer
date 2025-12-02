1682983f696122a9cb9e10f00fd055b2cd8e422a
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-12-02 14:26:17 +0300

    adding Resume Feedback page

---
+++ app/routes/resume.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { Link, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => [
+  { title: "Resumind | Review" },
+  { name: "description", content: "Detailed overview of your resume" },
+];
+
+const resume = () => {
+  const { auth, fs, isLoading, kv } = usePuterStore();
+  const { id } = useParams();
+  const [imageUrl, setImageUrl] = useState<string | null>("");
+  const [resumeUrl, setResumeUrl] = useState<string | null>("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
+  const navigate = useNavigate();
+
+     useEffect(() => {
+    if (!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
+    }, [isLoading]);
+
+
+  useEffect(() => {
+    const loadResume = async () => {
+      // Fetch resume data using the id
+      const resume = await kv.get(`resume-${id}`);
+
+      if (!resume) return;
+
+      const data = JSON.parse(resume);
+
+      const resumeBlob = await fs.read(data.resumePath);
+      if (!resumeBlob) return;
+
+      const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+      const resumeUrl = URL.createObjectURL(pdfBlob);
+      setResumeUrl(resumeUrl);
+
+      const imageBlob = await fs.read(data.imagePath);
+      if (!imageBlob) return;
+
+      const imageUrl = URL.createObjectURL(imageBlob);
+      setImageUrl(imageUrl);
+
+      setFeedback(data.feedback);
+      console.log({resumeUrl, imageUrl, feedback});
+    };
+
+    loadResume();
+  }, [id]);
+
+  return (
+    <main className="pt-0!">
+      <nav className="resume-nav">
+        <Link to="/" className="back-button">
+          <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+          <span className="text-gray-800 text-sm font-semibold">
+            Back to Home
+          </span>
+        </Link>
+      </nav>
+      <div className="flex flex-row w-full max-lg:flex-col-reverse">
+        <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[h-screen] sticky top-0 items-center justify-center">
+          {imageUrl && resumeUrl && (
+            <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-w-xl:h-fit w-fit">
+                <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+                    <img 
+                         src={imageUrl} 
+                         alt="" 
+                         className="w-full h-full object-contain rounded-2xl"
+                         title="resume"
+                    />
+                </a>
+            </div>
+          )}
+        </section>
+        <section className="feedback-section">
+            <h2 className="text-4xl text-black! font-bold">Resume Review</h2>
+            {feedback ? (
+                <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+                    <Summary  feedback={feedback}/>
+                    <ATS score={feedback.ATS.score || 0} suggetions={feedback.ATS.tips || []} />
+                    <Details feedback={feedback}/>
+                </div>
+            ) : (
+                <img src="/images/resume-scan-2.gif" alt="" className="w-full" />
+            )}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default resume;
